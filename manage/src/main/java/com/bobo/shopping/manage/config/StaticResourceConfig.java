26196566dc9ffbd21c71bf195463d7e63d9ae1a8
wuxiaobo <wuxiaobo@didachuxing.com>
2018-09-24 20:55:11 +0800

    测试themleaf和Controller跳转功能

---
+++ manage/src/main/java/com/bobo/shopping/manage/config/StaticResourceConfig.java
@@ -0,0 +1,27 @@
+package com.bobo.shopping.manage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 静态资源配置
+ *
+ * @author wuxiaobo@didachuxing.com
+ * @create 2018-09-24 19:27
+ **/
+
+@Configuration
+public class StaticResourceConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("classpath:/resources/")
+                .addResourceLocations("classpath:/static/")
+                .addResourceLocations("classpath:/public/");
+        super.addResourceHandlers(registry);
+    }
+
+}
---
+++ manage/src/main/java/com/bobo/shopping/manage/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.bobo.shopping.manage.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 登录，登出，首页功能
+ *
+ * @authorwuxiaobo@didachuxing.com
+ * @create 2018-09-23 21:14
+ **/
+@Controller
+@Slf4j
+public class LoginController {
+
+    /**
+     * 跳转到登录页面
+     * @return
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String loginPage() {
+        return "login";
+    }
+
+    /**
+     * 跳转到首页
+     * @return
+     */
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String indexPage() {
+        return "index";
+    }
+}
