23af496ce7dd7af5581c4ed0dc5780ef86f3c081
wuxiaobo <wuxiaobo@didachuxing.com>
2018-10-15 11:28:44 +0800

    引入验证码和guava

---
+++ manage/src/main/java/com/bobo/shopping/manage/config/Kaptcha/KaptchaConfig.java
@@ -0,0 +1,35 @@
+package com.bobo.shopping.manage.config.Kaptcha;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+/**
+ * 验证码
+ *
+ * @author wuxiaobo@didachuxing.com
+ * @create 2018-10-09 18:49
+ **/
+@Component
+public class KaptchaConfig {
+    @Bean
+    public DefaultKaptcha getDefaultKaptcha(){
+        com.google.code.kaptcha.impl.DefaultKaptcha defaultKaptcha = new com.google.code.kaptcha.impl.DefaultKaptcha();
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.border", "yes");
+        properties.setProperty("kaptcha.border.color", "105,179,90");
+        properties.setProperty("kaptcha.textproducer.font.color", "blue");
+        properties.setProperty("kaptcha.image.width", "110");
+        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.textproducer.font.size", "30");
+        properties.setProperty("kaptcha.session.key", "code");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+        Config config = new Config(properties);
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+}
--- manage/src/test/java/com/bobo/shopping/manage/ManageApplicationTests.java
+++ manage/src/test/java/com/bobo/shopping/manage/ManageApplicationTests.java
@@ -1,10 +1,16 @@
 package com.bobo.shopping.manage;
 
 import com.bobo.shopping.manage.common.ThreadPoolHelper;
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,6 +18,29 @@ public class ManageApplicationTests {
 
     @Test
     public void contextLoads() {
+        Optional<Integer> of = Optional.of(null);
+    }
+
+    @Test
+    public void testGuavaInt() {
+        List<Integer> list = new ArrayList<>();
+        Preconditions.checkArgument(list.size() >10,"wuxiaoob");
+    }
+    @Test
+    public void guava() {
+        Optional<Integer> absent = Optional.absent();
+        System.err.println("absent======"+absent.isPresent());
+        Optional<Integer> of = Optional.of(123);
+        Integer integer = of.get();
+        System.err.println(integer);
+        System.err.println(of.hashCode());
+        Set<Integer> set = of.asSet();
+        System.err.println(set.toString());
+        Optional<Class<Integer>> classOptional = Optional.fromNullable(Integer.class);
+        Class<Integer> integerClass = classOptional.get();
+        Package aPackage = integerClass.getPackage();
+        System.err.println(aPackage.getName());
+        System.err.println(classOptional.isPresent());
     }
 
     @Test
